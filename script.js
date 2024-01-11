2fb8762537233ecce6a89aadb5beea6137441d04
Roma <romaasieshvili2007@gmail.com>
2024-01-11 19:51:36 +0400

    javascript calculator first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+    // var pirveliCifri = 14;
+    // var meoreliCifri = 5;
+    // var jami = pirveliCifri + meoreliCifri
+    // var nakli = pirveliCifri - meoreliCifri
+    // var namravli = pirveliCifri * meoreliCifri
+    // console.log(jami)
+    // console.log(nakli)
+    // console.log(namravli)
+    // document.getElementById("jami").innerHTML = jami;
+    
+let mimateba1 = 34;
+let plus = "+";
+let mimateba2 = 17;
+let mimatebajami = mimateba1 + mimateba2;
+
+document.getElementById("mimateba1").innerHTML = mimateba1;
+document.getElementById("plus").innerHTML = plus;
+document.getElementById("mimateba2").innerHTML = mimateba2;
+document.getElementById("mimatebajami").innerHTML = mimatebajami;
+
+let saklebi = 100;
+let minus = "-";
+let maklebi = 20;
+let sxvaoba = saklebi - maklebi;
+
+document.getElementById("saklebi").innerHTML = saklebi;
+document.getElementById("minus").innerHTML = minus;
+document.getElementById("maklebi").innerHTML = maklebi;
+document.getElementById("sxvaoba").innerHTML = sxvaoba;
+
+let samravli = 10;
+let gamravleba = "*";
+let mamravli = 15;
+let namravli = samravli * mamravli;
+
+document.getElementById("samravli").innerHTML = samravli;
+document.getElementById("gamravleba").innerHTML = gamravleba;
+document.getElementById("mamravli").innerHTML = mamravli;
+document.getElementById("namravli").innerHTML = namravli;
+
+let asayvaniRicxvi = 4;
+let xarisxisNishani = "^";
+let xarisxi = 6;
+let pasuxi = asayvaniRicxvi ** xarisxi;
+
+document.getElementById("asayvaniRicxvi").innerHTML = asayvaniRicxvi;
+document.getElementById("xarisxisNishani").innerHTML = xarisxisNishani;
+document.getElementById("xarisxi").innerHTML = xarisxi;
+document.getElementById("pasuxi").innerHTML = pasuxi;
+
+let fudze = 125;
+let fesvi = "";
+let fesvisxarisxi = 3;
+let pasuxifesvi = Math.cbrt(fudze);
+
+document.getElementById("fudze").innerHTML = fudze;
+document.getElementById("fesvi").innerHTML = fesvi;
+document.getElementById("fesvisxarisxi").innerHTML = fesvisxarisxi;
+document.getElementById("pasuxifesvi").innerHTML = pasuxifesvi;
